fae05b15b1975f56e12ee8dfd2b635d678ea3d71
wujl12@lenovo.com <wujl12@lenovo.com>
2020-05-06 16:36:06 +0800

    文件的上传与下载,均在本地测试通过

--- file/src/main/java/com/lefile/file/controller/FileController.java
+++ file/src/main/java/com/lefile/file/controller/FileController.java
@@ -1,18 +1,20 @@
 package com.lefile.file.controller;
 
 import com.lefile.file.service.FileService;
+import com.lefile.file.utils.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 
-@RestController
+@Controller
 @RequestMapping("/file")
 public class FileController {
 
@@ -21,38 +23,54 @@ public class FileController {
 
     @RequestMapping("/upload")
     @ResponseBody
-    public String upload(@RequestParam("filefile") MultipartFile file, HttpServletRequest request) throws Exception {
-        String name = request.getParameter("name");
+    public JsonResult upload(@RequestParam("uploadFile") MultipartFile file, HttpServletRequest request) throws Exception {
+        String userName = request.getParameter("userName");
+        String projectName = request.getParameter("projectName");
+        return fileService.addFile(userName, projectName, file);
         //文件名
-        String fileName = file.getOriginalFilename();
-        System.out.println("文件名:" + fileName);
+//        String fileName = file.getOriginalFilename();
+//        System.out.println("文件名:" + fileName);
         //文件后缀
-        String suffixName = fileName.substring(fileName.lastIndexOf("."));
-        System.out.println("文件后缀名" + suffixName);
+//        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+//        System.out.println("文件后缀名" + suffixName);
         //创建文件
-        try {
-            File localFile = new File("D:\\apache", fileName);
-            file.transferTo(localFile);
-            return "success:" + localFile.getAbsolutePath();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "failed upload file";
+//        try {
+//            File localFile = new File("D:\\apache", fileName);
+//            file.transferTo(localFile);
+//            return "success:" + localFile.getAbsolutePath();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return "failed upload file";
     }
 
-    @GetMapping("/{id}")
+    @RequestMapping(value = "/download",method = RequestMethod.GET)
     public void download(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        File file = new File("");
-        FileInputStream fis = new FileInputStream(file);
-        response.setContentType("application/force-download");
-        response.addHeader("Content-disposition", "attachment;fileName=" + "a.txt");
-        OutputStream os = response.getOutputStream();
-        byte[] buf = new byte[1024];
-        int len = 0;
-        while ((len = fis.read(buf)) != -1) {
-            os.write(buf, 0, len);
+        String fileName = request.getParameter("fileName");
+        String projectName = request.getParameter("projectName");
+        String userName = request.getParameter("userName");
+//        File file = new File(String.format("D:\\%s\\%s\\%s", userName, projectName, fileName));
+        File file = new File("D:\\apache\\USBWriter.exe");
+        if (file.exists()) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+                response.setContentType("application/force-download");
+                response.addHeader("Content-disposition", "attachment;fileName=" + file.getName());
+                OutputStream os = response.getOutputStream();
+                byte[] buf = new byte[1024];
+                int len = 0;
+                while ((len = fis.read(buf)) != -1) {
+                    os.write(buf, 0, len);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                fis.close();
+            }
+        } else {
+            System.out.println("file doesn't exist:" + file.getAbsolutePath());
         }
-        fis.close();
     }
 
 
--- file/src/main/java/com/lefile/file/service/FileService.java
+++ file/src/main/java/com/lefile/file/service/FileService.java
@@ -1,19 +1,35 @@
 package com.lefile.file.service;
 
+import com.lefile.file.utils.JsonResult;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 public class FileService {
 
-    public boolean addFile(){
+    public JsonResult addFile(String userName, String projectName, MultipartFile file) {
+        try {
+            File file_local = new File("D:\\apache", file.getOriginalFilename());
+            file.transferTo(file_local);
+            return JsonResult.ok("path:" + file_local.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return JsonResult.errorMsg("file upload failed");
+    }
+
+    public boolean addFile() {
         return true;
     }
 
-    public boolean deleteFile(String path){
+    public boolean deleteFile(String path) {
         return true;
     }
 
-    public boolean getFile(String path){
+    public boolean getFile(String path) {
         return true;
     }
 
